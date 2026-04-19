9ffbb708534bd6ad5d71a33ed39ebb913a696269
Arif Ullah <arifkhattak736@gmail.com>
2026-04-19 23:50:40 +0500

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
-import "./App.css";
+// import "./App.css";
 import SideBar from "./components/SideBar";
 import Player from "./components/Player";
+import Display from "./components/Display";
 
 function App() {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <SideBar />
+        <Display />
       </div>
       <Player />
     </div>
